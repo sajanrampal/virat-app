996222d767a65d8b7c2c5490cb48b9b4fe18205e
sajanrampal <rampalsajjan@gmail.com>
2021-01-27 23:13:52 +0530

    feat: added the ui app on root

--- apps/virat-backend-app/src/main.ts
+++ apps/virat-backend-app/src/main.ts
@@ -5,13 +5,14 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import * as express from 'express';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api/v1';
   app.setGlobalPrefix(globalPrefix);
+  app.use('/',express.static("../../ui-app"))
   const port = process.env.PORT || 4500;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
