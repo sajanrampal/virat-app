6d2e8ff8e118a03a31b0fc8586075d932cb9b2eb
sajanrampal <35993744+sajanrampal@users.noreply.github.com>
2021-01-29 19:07:14 +0530

    Update app.service.ts
    refactor:testing commit on heroku

--- apps/virat-backend-app/src/app/app.service.ts
+++ apps/virat-backend-app/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Welcome to virat-backend-app! Thanks for visiting' };
+    return { message: 'Welcome to virat-backend-app!' };
   }
 }
